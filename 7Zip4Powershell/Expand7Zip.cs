88828392557426692c8c92b37ad759852c521540
Isaac Springer <isaac.springer@ideas.com>
2016-01-06 12:37:46 -0600

    Add exist check and force param

--- 7Zip4Powershell/Expand7Zip.cs
+++ 7Zip4Powershell/Expand7Zip.cs
@@ -10,9 +10,12 @@ namespace SevenZip4Powershell {
         [ValidateNotNullOrEmpty]
         public string ArchiveFileName { get; set; }
 
-        [Parameter(Position = 1, Mandatory = true, HelpMessage = "The target folder")]
-        [ValidateNotNullOrEmpty]
-        public string TargetPath { get; set; }
+		[Parameter(Position = 1, Mandatory = true, HelpMessage = "The target folder")]
+		[ValidateNotNullOrEmpty]
+		public string TargetPath { get; set; }
+
+		[Parameter(Position = 2, Mandatory = false, HelpMessage = "Force extraction to target folder")]
+		public SwitchParameter Force { get; set; }
 
         protected override CmdletWorker CreateWorker() {
             return new ExpandWorker(this);
@@ -31,25 +34,30 @@ namespace SevenZip4Powershell {
 
                 var targetPath = new FileInfo(Path.Combine(_cmdlet.SessionState.Path.CurrentFileSystemLocation.Path, _cmdlet.TargetPath)).FullName;
                 var archiveFileName = new FileInfo(Path.Combine(_cmdlet.SessionState.Path.CurrentFileSystemLocation.Path, _cmdlet.ArchiveFileName)).FullName;
+				var activity = String.Format ("Extracting {0} to {1}", System.IO.Path.GetFileName (archiveFileName), targetPath);
 
-                var activity = String.Format("Extracting {0} to {1}", System.IO.Path.GetFileName(archiveFileName), targetPath);
-                var statusDescription = "Extracting";
+				if (!Directory.Exists (targetPath) || this._cmdlet.Force) {
+					var statusDescription = "Extracting";
 
-                Write(String.Format("Extracting archive {0}", archiveFileName));
-                WriteProgress(new ProgressRecord(0, activity, statusDescription) { PercentComplete = 0 });
+					Write (String.Format ("Extracting archive {0}", archiveFileName));
+					WriteProgress (new ProgressRecord (0, activity, statusDescription) { PercentComplete = 0 });
 
-                using (var extractor = new SevenZipExtractor(archiveFileName)) {
-                    extractor.Extracting += (sender, args) =>
-                                            WriteProgress(new ProgressRecord(0, activity, statusDescription) { PercentComplete = args.PercentDone });
-                    extractor.FileExtractionStarted += (sender, args) => {
-                        statusDescription = String.Format("Extracting file {0}", args.FileInfo.FileName);
-                        Write(statusDescription);
-                    };
-                    extractor.ExtractArchive(targetPath);
-                }
+					using (var extractor = new SevenZipExtractor (archiveFileName)) {
+						extractor.Extracting += (sender, args) =>
+                                            WriteProgress (new ProgressRecord (0, activity, statusDescription) { PercentComplete = args.PercentDone });
+						extractor.FileExtractionStarted += (sender, args) => {
+							statusDescription = String.Format ("Extracting file {0}", args.FileInfo.FileName);
+							Write (statusDescription);
+						};
+						extractor.ExtractArchive (targetPath);
+					}
 
-                WriteProgress(new ProgressRecord(0, activity, "Finished") { RecordType = ProgressRecordType.Completed });
-                Write("Extraction finished");
+					WriteProgress (new ProgressRecord (0, activity, "Finished") { RecordType = ProgressRecordType.Completed });
+					Write ("Extraction finished");
+				} else {
+					WriteProgress (new ProgressRecord (0, activity, "Finished") { RecordType = ProgressRecordType.Completed });
+					Write ("Extraction finished: Target already exists");
+				}
             }
         }
     }
